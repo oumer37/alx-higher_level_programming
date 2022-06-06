accb452ca16088a933950a270e16da51113d7189
root <oumich37@gmail.com>
2022-06-05 20:54:40 -0700

    e

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    my_list = input("enter the list:")
-    idx = []
-    print("the lists: ", " {} ".format(my_list))
-element_at(my_list, idx)
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
